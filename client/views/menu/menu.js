46a1cb4b6ab690d9acada3998bbcfa8d78cc6e0d
Tim Banks <thetimbanks@gmail.com>
2015-02-14 16:43:02 -0600

    Add order form modal for ordering drinks

--- client/views/menu/menu.js
+++ client/views/menu/menu.js
@@ -1,7 +1,21 @@
 Template.menu.events({
   'click .order': function(event) {
+    Session.set("currentDrink", $(event.target).data("id"));
+    $('#order-modal').modal("show");
+    return false;
+  }
+});
+
+Template.orderForm.rendered = function() {
+  $('#order-modal').on('hidden.bs.modal', function (e) {
+    Session.set("currentDrink", null);
+  })
+}
+
+Template.orderForm.events({
+  'submit form': function(event) {
     var order_params = {
-      drink: Drinks.findOne($(event.target).data("id")),
+      drink: Drinks.findOne(Session.get("currentDrink")),
       ready: false,
       user_id: Meteor.userId(),
       user_avatar: Meteor.user().services.google.picture
@@ -9,8 +23,15 @@ Template.menu.events({
 
     Meteor.call("add_order", order_params, function() {
       FlashMessages.sendSuccess("Your drink has been ordered");
+      $('#order-modal').modal("hide");
     });
 
     return false;
+  }
+});
+
+Template.orderForm.helpers({
+  drink: function() {
+    return Drinks.findOne(Session.get("currentDrink"));
   }
 });
